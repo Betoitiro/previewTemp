445af2ae58df423bc5f1a0438c520174b6640444
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-05-09 23:01:23 -0300

    alteraações

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import json
 
 api_key = 'f4fd48a14fbc3515ce08b19319cb6a16'
 
-url = f'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=f4fd48a14fbc3515ce08b19319cb6a16'
-response = requests.get(url)
-response = response.json()
+
 
 list = []
 
-for i in range(10):
-    list.append(response)
+lat = float
+lon = float
+
+for lon in range(10):   
+    for lat in range(10):
+        url = f'https://api.openweathermap.org/data/2.5/weather?id={cityid}&appid=f4fd48a14fbc3515ce08b19319cb6a16'
+        response = requests.get(url)
+        response = response.json()
+
+        list.append(response)
 
 print(list)
